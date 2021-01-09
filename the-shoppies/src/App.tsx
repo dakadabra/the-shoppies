c6ee0d93a3ac7352dc3a5de2f2f86a867c15432e
dakadabra <david.a.khazzam@gmail.com>
2021-01-09 15:33:11 -0500

    added basic poster size styling

--- the-shoppies/src/App.tsx
+++ the-shoppies/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NominationsList } from './NominationsList';
 import { SearchBar } from './SearchBar';
+import './styles.css';
 
 /*
 Done:
